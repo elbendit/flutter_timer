9f706627b320764e34e3cecd333e98a54144cedb
elbendit <jsuarez0601@gmail.com>
2023-06-22 05:18:52 -0500

    State and events

---
+++ lib/ticker.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+class Ticker {
+  const Ticker();
+  Stream<int> tick({required int ticks}) {
+    return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
+  }
+}
